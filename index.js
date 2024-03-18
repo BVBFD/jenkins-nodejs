01214afee9a38dd8a50645ff1600a6715d44d976
lsevina126 <lsevina126@gmail.com>
2024-03-18 17:29:39 +0900

    Feat(mocha,dotenv): Added features of mocha test and dotenv

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 "use strict";
-
+const dotenv = require("dotenv");
 const express = require("express");
 
-// Constants
-const PORT = 3000;
-const HOST = "0.0.0.0";
+dotenv.config();
 
-// App
+const { HOST, PORT } = process.env;
+
 const app = express();
-// app.get('/', (req, res) => {
-//   res.send('Hello World');
-// });
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/public/index.html");
@@ -23,5 +19,3 @@ app.get("/hello", function (req, res) {
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
 });
-
-/////https://www.youtube.com/watch?v=vm3YfOHf_Cc
